0bbf38f5b0a731765b62c9877cd6b2a27b7891e1
BNivia <nobenjamin19@gmail.com>
2020-11-17 23:10:39 -0500

    Ex 5 completed

--- world.js
+++ world.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
     var button = document.getElementById("lookup");
+    var button2 = document.getElementById("city-lookup");
     var key = document.getElementById("country").value;
     
 
@@ -20,5 +21,26 @@ document.addEventListener('DOMContentLoaded', function(){
         }
         httpR.open('GET', "http://localhost/info2180-lab5/world.php?country="+key, true);
         httpR.send();
-    });    
+    });
+    
+    button2.addEventListener('click', (event) => {
+        event.preventDefault();        
+        httpR = new XMLHttpRequest();
+        
+        httpR.onreadystatechange = function(){
+            if (httpR.readyState === XMLHttpRequest.DONE && httpR.status === 200){
+                var r = httpR.responseText;
+                var key = document.getElementById("country").value;
+                //alert(r);
+                document.getElementById("result").innerHTML = r;
+            }
+            if (httpR.readyState === XMLHttpRequest.DONE && httpR.status === 404){
+                alert('ERROR - File not found.');
+            }
+        }
+        httpR.open('GET', "http://localhost/info2180-lab5/world.php?country="+key+"&context=cities", true);
+        httpR.send();
+    }); 
+    
+
 });
--- world.php
+++ world.php
@@ -7,16 +7,45 @@ $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 $country=filter_var($_REQUEST['country'], FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-if ($country != null || $country != "" ){
-  $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
-  $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-} 
-else{
-  $stmt = $conn->query("SELECT * FROM countries");
-  $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$context=filter_var($_REQUEST['context'], FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+
+$searchby = "none";
+
+if ($context != "cities"){
+  if ($country != null || $country != ""){
+    $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $searchby = "con";
+    }else{
+      $stmt = $conn->query("SELECT * FROM countries");
+      if ($stmt != null || $stmt != ""){
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $searchby = "con";
+      }else{
+        $searchby = "none";
+        $result = "Not found.";
+    }
+  }
+}
+
+
+if ($context == "cities"){
+  if ($country != null || $country != ""){
+    $stmt = $conn->query("SELECT cities.name, cities.district, cities.population FROM cities JOIN countries ON cities.country_code=countries.code WHERE countries.name LIKE '%$country%'");
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $searchby = "cit";
+  }else{
+    $stmt = $conn->query("SELECT cities.name, cities.district, cities.population FROM cities JOIN countries ON cities.country_code = countries.code;");
+    if ($stmt != null || $stmt != ""){
+      $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }else{
+      $result = "Not found.";
+    }
+  }
 }
 ?>
 
+<?php if ($searchby == "con"):?>
 <table>
     <tr>
       <th>Name</th>
@@ -33,4 +62,25 @@ else{
       </tr>
     <?php endforeach; ?>
 </table>
+<?php endif; ?>
 
+<?php if ($searchby == "cit"): ?>
+  <table>
+    <tr>
+      <th>Name</th>
+      <th>District</th>
+      <th>Population</th>
+    </tr>
+    <?php foreach ($result as $row): ?>
+      <tr>
+      <td><?= $row['name'] ?></td>
+      <td><?= $row['district'] ?></td>
+      <td><?= $row['population'] ?></td>
+      </tr>
+    <?php endforeach; ?>
+</table>
+<?php endif; ?>
+
+<?php if ($searchby == "none" && $result == "Not found."): ?>
+  <p><?= $result ?></p>
+<?php endif; ?>
